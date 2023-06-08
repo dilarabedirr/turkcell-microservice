a030c9b9c6ebecfc1e8d7c777465d5db68bd3342
Dilara BEDİR <dilara_bedir@hotmail.com>
2023-06-08 22:30:15 +0300

    add zipkin, prometheus and grafana

--- common-package/src/main/java/com/kodlama/io/commonpackage/configuration/security/SecurityConfig.java
+++ common-package/src/main/java/com/kodlama/io/commonpackage/configuration/security/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
         converter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtRoleConverter());
 
         http.cors().and().authorizeHttpRequests()
-                .requestMatchers("/api/filters", "/api/cars/check-car-available/**", "/api/payments/check")
+                .requestMatchers("/api/filters", "/api/cars/check-car-available/**", "/api/payments/check","/actuator/**")
                 .permitAll()
                 .requestMatchers("/api/**")
                 .hasAnyRole("user")
